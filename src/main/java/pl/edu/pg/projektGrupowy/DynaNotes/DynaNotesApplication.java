0b1140c9d6b82eb7453afc1f8167af7be7e0d6df
Piotr Wawreniuk <piotr21wawreniuk@gmail.com>
2022-11-23 18:45:36 +0100

    initialize application

---
+++ src/main/java/pl/edu/pg/projektGrupowy/DynaNotes/DynaNotesApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pg.projektGrupowy.DynaNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynaNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynaNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/pg/projektGrupowy/DynaNotes/DynaNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pg.projektGrupowy.DynaNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynaNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
