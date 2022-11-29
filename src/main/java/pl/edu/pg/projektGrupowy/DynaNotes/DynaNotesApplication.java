31ec575f06554673d7b34446ce56bcf273a913e3
Piotr Wawreniuk <piotr21wawreniuk@gmail.com>
2022-11-29 18:24:23 +0100

    add Note data object with REST controller

--- src/main/java/pl/edu/pg/projektGrupowy/DynaNotes/DynaNotesApplication.java
+++ src/main/java/pl/edu/pg/projektGrupowy/DynaNotes/DynaNotesApplication.java
@@ -1,6 +1,6 @@
 package pl.edu.pg.projektGrupowy.DynaNotes;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication; 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
