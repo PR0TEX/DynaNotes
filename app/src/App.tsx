318d288fd667b841d019dd68cdc9a660c14afa90
Varker00 <lukaszhein00@gmail.com>
2022-12-18 16:24:26 +0100

    Added bare-bones edit functionality

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState, useReducer } from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import { v4 as uuid } from 'uuid';
+import { nodeModuleNameResolver, updateUnionTypeNode } from 'typescript';
 
 const initialNotesState = {
     lastNoteCreated: null,
@@ -29,6 +30,14 @@ const notesReducer = (prevState: any, action: any) => {
             };
             console.log('After DELETE_NOTE: ', newState);
             return newState;
+        }
+
+        case 'EDIT_NOTE': {
+            //changing internal note text
+            prevState.notes.filter((note: { id: any; }) => note.id === action.payload.id)[0].text="test";
+
+            console.log('After EDIT_NOTE: ', prevState);
+            return prevState;
         }
     }
 };
@@ -59,9 +68,70 @@ export function App() {
     }
 
     const dropNote = (event: any) => {
+        console.log(event);
         event.target.style.left = `${event.pageX - 50}px`;
         event.target.style.top = `${event.pageY - 50}px`;
     };
+
+    //TODO save and end all edits
+    const endEdit = (event: any) => {
+        event.preventDefault();
+        
+        let elems = Array.from(document.getElementsByTagName("textarea"));
+
+        elems.forEach(textarea => {
+            let note = textarea.parentElement;
+            if(note === null || note === undefined || note.className !== "note") return;
+
+            let pre = document.createElement("pre");
+
+            pre.className = "text";
+            pre.innerHTML = textarea.value;
+
+            note.removeChild(textarea);
+            note.appendChild(pre);
+
+            //update note
+        });
+    }
+
+    const startEditing = (event: any) =>{
+
+        //find note element
+        let note = event.target;
+        while(note.className !== "note"){
+            note = note.parentElement;
+        }
+
+        let pre = note.getElementsByTagName("pre")[0];
+        console.log(pre)
+
+        //end editing
+        if(pre === undefined || pre === null){
+            let pre = document.createElement("pre");
+            let textarea = note.getElementsByTagName("textarea")[0];
+
+            pre.className = "text"
+            pre.innerHTML = textarea.value;
+
+            note.removeChild(textarea);
+            note.appendChild(pre);
+
+            //update note state
+            //dispatch({ type: 'UPDATE_NOTE', payload: })
+        }
+
+        //start editing
+        else{
+            let textarea = document.createElement("textarea");
+            textarea.value = pre.innerHTML;
+            textarea.className = "editBox";
+
+            note.removeChild(pre);
+            note.appendChild(textarea);
+        }
+        
+    }
 
     return (
         <div className="app" onDragOver={dragOver}>
@@ -75,7 +145,7 @@ export function App() {
                     value={noteInput}
                     onChange={event => setNoteInput(event.target.value)}>
                 </textarea>
-                <button>Add</button>
+                <button type="submit">Add</button>
             </form>
 
             {notesState
@@ -85,12 +155,22 @@ export function App() {
                         style={{ transform: `rotate(${note.rotate}deg)` }}
                         onDragEnd={dropNote}
                         draggable="true"
+                        onDoubleClick={startEditing}
+                        id={note.id?.toString()}
                         key={note.id}>
 
                         <div onClick={() => dispatch({ type: 'DELETE_NOTE', payload: note })}
                             className="close">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
+                            </svg>
+                        </div>
+
+                        <div onClick={startEditing}
+                            className="edit">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round">
+                                <path d="M20 14.66V20a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h5.34"/>
+                                <polygon points="18 2 22 6 12 16 8 16 8 12 18 2"/>
                             </svg>
                         </div>
 
