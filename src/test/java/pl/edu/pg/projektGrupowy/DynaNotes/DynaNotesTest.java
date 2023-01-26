3fd7584db5dcc85f08b1e0bc91e4e7ecef9d870f
Domaradzki <pawel@DT-HBJ6P13.localdomain>
2023-01-26 07:47:05 +0100

    selenium tests

--- src/test/java/pl/edu/pg/projektGrupowy/DynaNotes/DynaNotesTest.java
+++ src/test/java/pl/edu/pg/projektGrupowy/DynaNotes/DynaNotesTest.java
@@ -2,23 +2,26 @@ package pl.edu.pg.projektGrupowy.DynaNotes;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 import org.junit.After;
-import org.junit.Before;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.springframework.boot.test.context.SpringBootTest;
 
 class DynaNotesTest {
 
 	public WebDriver getChromeBrowser() {
-        //TODO change location
-        System.setProperty("webdriver.chrome.driver", "/usr/src/app/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/home/driver/chromedriver");
 
         return new ChromeDriver(getChromeHeadlessOptions());
     }
@@ -34,10 +37,10 @@ class DynaNotesTest {
 	
 	private WebDriver driver;
 	
-	@Before
+	@BeforeEach
 	void setUp() {
 		driver = this.getChromeBrowser();
-		driver.get(System.getProperty("http://localhost:3000/")); //TODO depends on implementation
+		driver.get(System.getProperty("http://localhost:3000/"));
 	}
 
 	@After
@@ -47,58 +50,86 @@ class DynaNotesTest {
 		}
 	}
 
-	//Zmiana koloru zła
-
 	@Test
 	public void whenStickyNoteIsCreatedThenItIsPinnedToTheBoard() {
-		//Depends from number of created notes check instances of class note_yellow
 		driver.findElement(Selectors.CreateStickyNoteSelector.getBy()).sendKeys("TEST");
-		// driver.findElement(Selectors.AddStickyNoteButtonSelector.getBy()).click();
+		driver.findElement(Selectors.AddStickyNoteButtonSelector.getBy()).click();
 
-		// try{
-			// driver.findElement(Selectors.YellowStickyNoteSelector.getBy());
-		// }catch (NoSuchElementException e){
-			// throw new NoSuchElementException();
-		// }
-		//TODO generate new element with id
-		// assertThat(driver.findElement(Selectors.CreatedElementSelector.getBy()));
+		try{
+			driver.findElement(Selectors.YellowStickyNoteSelector.getBy());
+		}catch (NoSuchElementException e){
+			Assert.fail("Note not added");
+		}
 	}
-	//CO JAK KARTECZKI NIE MA
-	// @Test
-	// public void whenNoteContentIsEditedThenContentChanges() {
-	// 	String edited_content="edited Content";
-	// 	driver.findElement(Selectors.EditStickyNoteSelector.getBy()).click();
-	// 	driver.findElement(Selectors.EditStickyNoteContentSelector.getBy()).sendKeys(edited_content);
 
-	// 	assertEquals(driver.findElement(Selectors.EditStickyNoteContentSelector.getBy()).getText(),edited_content);
-	// }
-	// String getStyle(String color) {
-	// 	return "background " + color;
-	// }
-	// String getRandomColor(String colors[]) {
-	// 	int index = new Random().nextInt(colors.length);
-	// 	return colors[index];
-	// }
-	// @Test
-	// public void whenNoteColorIsChangedThenNoteHasNewColor() {
-	// 	String colors[] = {"yellow", "cyan", "green", "magenta", "orange", "purple"};
-	// 	//TODO add color-style info to the note
-	// 	String prevNoteColor = driver.findElement(Selectors.StickyNoteSelector.getBy()).getAttribute("style");
-	// 	String color = getRandomColor(colors.remove(prevNoteColor));
+	@Test
+	public void whenNoteContentIsEditedThenContentChanges() {
+		String edited_content="edited Content";
+		driver.findElement(Selectors.YellowStickyNoteSelector.getBy()).click();
+		driver.findElement(Selectors.EditStickyNoteContentSelector.getBy()).sendKeys(edited_content);
 
-	// 	driver.findElement(Selectors.ChangeColorButtonSelector.getBy()).click();
-	// 	WebElement noteColorElement = getRandomColorElement(color);
-	// 	noteColorElement.click();
+	 	assertEquals(driver.findElement(Selectors.EditStickyNoteContentSelector.getBy()).getText(),edited_content);
+	}
 
-	// 	assertEquals(driver.findElement(Selectors.StickyNoteSelector.getBy()).getAttribute("style"), getStyle(color));
+	@Test
+	public void whenNoteIsDeletedThenNoteDisappears() {
+	 	driver.findElement(Selectors.DeleteStickyNoteSelector.getBy()).click();
+		
+		try{
+			driver.findElement(Selectors.YellowStickyNoteSelector.getBy());
+		}catch(NoSuchElementException e){
+			return;
+		}
+		Assert.fail("The note has not been deleted");
 
-	// }
+	}
+	
+	@Test
+	public void whenNoteColorIsChangedThenNoteHasNewColor() {
+		String prevNoteColor = driver.findElement(Selectors.YellowStickyNoteSelector.getBy()).getAttribute("class").split(" ")[0];
+		ArrayList<String> colors = getAvailableColors(prevNoteColor);
+		String color = getRandomColor(colors);
 
-	// @Test
-	// public void whenNoteIsDeletedThenNoteDisappears() {
-	// 	//Generate id and check if is dissapear
-	// 	driver.findElement(Selectors.DeleteStickyNoteSelector.getBy()).click();
+		driver.findElement(Selectors.ChangeColorButtonSelector.getBy()).click();
+		WebElement noteColorElement = getRandomColorElement(color).get();
+		try{
+			noteColorElement.click();
+		}catch(NoSuchElementException e){
+			Assert.fail("Note with new color is not available");
+		}
+	}
 
-	// }
+	ArrayList<String> getAvailableColors(String color){
+		String colors[] = {"yellow", "cyan", "green", "magenta", "orange", "purple"};
+		ArrayList<String>  availableColors = (ArrayList<String>) Arrays.stream(colors).collect(Collectors.toList());
+		availableColors.remove(color);
 
+		return availableColors;
+	}
+
+	String getRandomColor(ArrayList<String> colors) {
+		int index = new Random().nextInt(colors.size() - 1);
+		return colors.get(index);
+	}
+
+	Optional<WebElement> getRandomColorElement(String color) {
+		return driver.findElements(Selectors.NoteColorSelector.getBy())
+			.stream()	
+			.filter(noteColor -> noteColor.getAttribute("style").equals(getStyle(color)))	
+			.findFirst();	
+	}
+
+	String getStyle(String color) {
+		return "background: " + color;
+	}
+
+	@Test
+	public void whenDarkModeIsSelectedThenBackgroundColorChanges() {
+		driver.findElement(Selectors.ChangeThemeSelector.getBy()).click();
+		try{
+			driver.findElement(Selectors.DarkBoardThemeSelector.getBy());
+		}catch(NoSuchElementException e){
+			Assert.fail("The color theme has not been changed");
+		}
+	}
 }
--- src/test/java/pl/edu/pg/projektGrupowy/DynaNotes/Selectors.java
+++ src/test/java/pl/edu/pg/projektGrupowy/DynaNotes/Selectors.java
@@ -5,11 +5,14 @@ import org.openqa.selenium.By;
 @Getter
 public enum Selectors {
     CreateStickyNoteSelector(By.id("create_new_note")),
-    AddStickyNoteButtonSelector(By.id("add_note_button"));
-    // YellowStickyNoteSelector(By.className("note yellow")),
-    //TODO change id
-    // EditStickyNoteSelector(By.id("note1_")),
-    // EditStickyNoteContentSelector(By.className("editBox"));
+    AddStickyNoteButtonSelector(By.id("add_note_button")),
+    YellowStickyNoteSelector(By.className("note yellow")),
+    EditStickyNoteContentSelector(By.className("editBox")),
+    ChangeColorButtonSelector(By.className("palette")),
+    NoteColorSelector(By.className("colorPicker")),
+    DeleteStickyNoteSelector(By.className("close")),
+    ChangeThemeSelector(By.className("slider")),
+    DarkBoardThemeSelector(By.className("dark"));
     private final By by;
     Selectors(By by) {
           this.by = by;
